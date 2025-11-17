151cf49d60ac3f6113f1b6a2e7f6b3895f036edf
bruce wayne <imbatman@iusevimbtw.com>
2025-11-17 07:17:45 -0300

    feat: ui with react+vite+shadcn

--- apps/web/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	//import { MyCounterButton } from '@repo/ui';
-</script>
-
-<h1>Web</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
